c40ab4e795ab615d161e6d0f9d85b90c9edb529a
ArsenZar <arsenzar@gmail.com>
2025-03-19 19:05:11 +0200

    practice-5 continue...

---
+++ js/task-1.js
@@ -0,0 +1,59 @@
+
+
+/* practice-5 */
+/* this example shows how to get keys and values from an object if we don`t know the keys and values */
+/* 
+const apartment = {
+  descr: "Spacious apartment in the city center",
+  rating: 4,
+  price: 2153,
+};
+
+const keys = [];
+const values = [];
+
+for (const key in apartment){
+  keys.push(key);
+  values.push(apartment[key]);
+}
+
+console.log(keys);
+console.log(values);
+*/
+
+/* practice-4 */
+/*
+const emailInputName = "email";
+const passwordInputName = "password";
+
+const credentials = {
+  [emailInputName]: "henry.carter@aptmail.com",
+  [passwordInputName]: "jqueryismyjam",
+};
+*/
+/*
+--- practice
+
+const book = {
+  title: "The Last Kingdom",
+  author: "Bernard Cornwell",
+  genres: ["historical prose", "adventure"],
+  isPublic: true,
+  rating: 8.38,
+};
+
+book.pageCount = 836;
+book.originalLanguage = "en";
+book.translations = ["ua", "ru"];
+book.price = {
+  hardcover: 39,
+  softcover: 29,
+};
+
+console.log(book);
+console.log(book.pageCount); // 836
+console.log(book.originalLanguage); // "en"
+console.log(book.translations); // ["ua", "ru"]
+
+
+*/
