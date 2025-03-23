353375131fd311aa0a25f9f1befb2e650c056094
ArsenZar <arsenzar@gmail.com>
2025-03-23 23:23:01 +0200

    task-1 is ready

--- js/task-1.js
+++ js/task-1.js
@@ -1,4 +1,31 @@
-/* create dublicate object by ...spread */
+function isEnoughCapacity(products, containerSize){
+  let takeValues = Object.values(products);
+  let result = 0;
+
+  for (let i of takeValues) {
+    result += i;
+  }
+
+  return result < containerSize ? true : false;
+}
+
+console.log(
+  isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
+); // true
+
+console.log(
+  isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
+); // false
+
+console.log(
+  isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)
+); // true
+
+console.log(
+  isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)
+); // false
+
+/* practice 18 (create dublicate object by ...spread) */
 /*
 const defaultSettings = {
   theme: "light",
